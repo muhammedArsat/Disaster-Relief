155431bd67cc700c8e0747f54c7f0426593d6476
muhammedArsat <ars21.code@gmail.com>
2024-12-22 21:53:21 +0530

    folder structure

---
+++ frontend/src/pages/user/landing/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SkeletonCard from "../../../skeleton/skeletonCard/SkeletonCard";
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Landing Page</h1>
+      <SkeletonCard />
+    </div>
+  );
+};
+
+export default LandingPage;
---
+++ frontend/src/skeleton/skeletonCard/SkeletonCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SkeletonCard = () => {
+  return (
+    <div>
+      <h1>Skeleton Card</h1>
+    </div>
+  )
+}
+
+export default SkeletonCard
