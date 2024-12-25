ceb1de9f336b9279fed3e0d276267e125c628aae
muhammedArsat <ars21.code@gmail.com>
2024-12-25 14:45:30 +0530

    weather card update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,55 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
-import LandingPage from "./pages/user/landingPage/LandingPage";
-import Navbar from "./components/navbar/Navbar";
-import NotFound from "./pages/NotFound";
-import Test from "./Test"
-import LocationPage from "./pages/user/locationPage/Location"
-import Dashboard from "./pages/user/dashboard/Dashboard";
+import React from 'react';
+import './App.css';
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    useLocation,
+} from 'react-router-dom';
+import LandingPage from './pages/user/landingPage/LandingPage';
+import Navbar from './components/navbar/Navbar';
+import NotFound from './pages/NotFound';
+import Test from './Test';
+import LocationPage from './pages/user/locationPage/Location';
+import Dashboard from './pages/user/dashboard/Dashboard';
 
 const App = () => {
-  return (
-    <Router>
-      <NavBarWrapper />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/user/location" element={<LocationPage />} />
-        <Route path="/user/dashboard" element={<Dashboard/>} />
-        <Route path="/user/test" element={<Test/>}/>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <NavBarWrapper />
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/user/location" element={<LocationPage />} />
+                <Route path="/user/dashboard" element={<Dashboard />} />
+                <Route path="/user/test" element={<Test />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </Router>
+    );
 };
 
 // Single Navbar for all pages
 const NavBarWrapper = () => {
-  const location = useLocation();
-  const path = location.pathname;
+    const location = useLocation();
+    const path = location.pathname;
 
-  let navbarProps = {
-    isLogin: false,
-    isNotFound: false,
-    isAdmin: false,
-  };
+    let navbarProps = {
+        isLogin: false,
+        isNotFound: false,
+        isAdmin: false,
+    };
 
-  if (path === "/") {
-    navbarProps.isLogin = true;
-  } else if (path.startsWith("/user")) {
-    navbarProps.isAdmin = false;
-  } else if (path.startsWith("/admin")) {
-    navbarProps.isAdmin = true;
-  } else {
-    navbarProps.isNotFound = true;
-  }
+    if (path === '/') {
+        navbarProps.isLogin = true;
+    } else if (path.startsWith('/user')) {
+        navbarProps.isAdmin = false;
+    } else if (path.startsWith('/admin')) {
+        navbarProps.isAdmin = true;
+    } else {
+        navbarProps.isNotFound = true;
+    }
 
-  return <Navbar {...navbarProps} />;
+    return <Navbar {...navbarProps} />;
 };
 
 export default App;
