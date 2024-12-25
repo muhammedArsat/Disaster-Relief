d9a8fab36993ab38d2da918db3b6946915049f01
hamsa-09 <hamsavardhinibaskar@gmail.com>
2024-12-25 19:08:07 +0530

    home feed

---
+++ frontend/src/pages/user/home/Home.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import "./Home.css";
+import HomeFeed from "../../../components/user/homefeed/HomeFeed";
+import img1 from "../../../../assets/kc-bk-hero.jpg";
+
+const Home = () => {
+  const feeds = [
+    {
+      place: "Egmore, Chennai",
+      image: img1,
+      disaster: "Flood",
+      description:
+        "Severe flooding in chennai - egmore has displaced over 10,000 families, leaving them in urgent need of shelter and aid.Severe flooding in chennai - egmore has displaced over 10,000 families, leaving them in urgent need of shelter and aid.Severe flooding in chennai - egmore has displaced over 10,000 families, leaving them in urgent need of shelter and aid.Severe flooding in chennai - egmore has displaced over 10,000 families, leaving them in urgent need of shelter and aid.",
+      date: "2024-12-24",
+    },
+    {
+      place: "Egmore, Chennai",
+      image: img1,
+      disaster: "Flood",
+      description:
+        "Severe flooding in chennai - egmore has displaced over 10,000 families, leaving them in urgent need of shelter and aid.",
+      date: "2024-12-24",
+    },
+    {
+      place: "Egmore, Chennai",
+      image: img1,
+      disaster: "Flood",
+      description:
+        "Severe flooding in chennai - egmore has displaced over 10,000 families, leaving them in urgent need of shelter and aid.",
+      date: "2024-12-24",
+    },
+    {
+      place: "Egmore, Chennai",
+      image: img1,
+      disaster: "Flood",
+      description:
+        "Severe flooding in chennai - egmore has displaced over 10,000 families, leaving them in urgent need of shelter and aid.",
+      date: "2024-12-24",
+    },
+    {
+      place: "Egmore, Chennai",
+      image: img1,
+      disaster: "Flood",
+      description:
+        "Severe flooding in chennai - egmore has displaced over 10,000 families, leaving them in urgent need of shelter and aid.",
+      date: "2024-12-24",
+    },
+    {
+      place: "Egmore, Chennai",
+      image: img1,
+      disaster: "Flood",
+      description:
+        "Severe flooding in chennai - egmore has displaced over 10,000 families, leaving them in urgent need of shelter and aid.",
+      date: "2024-12-24",
+    },
+    {
+      place: "Egmore, Chennai",
+      image: img1,
+      disaster: "Flood",
+      description:
+        "Severe flooding in chennai - egmore has displaced over 10,000 families, leaving them in urgent need of shelter and aid.",
+      date: "2024-12-24",
+    },
+    {
+      place: "Egmore, Chennai",
+      image: img1,
+      disaster: "Flood",
+      description:
+        "Severe flooding in chennai - egmore has displaced over 10,000 families, leaving them in urgent need of shelter and aid.",
+      date: "2024-12-24",
+    },
+    {
+      place: "Egmore, Chennai",
+      image: img1,
+      disaster: "Flood",
+      description:
+        "Severe flooding in chennai - egmore has displaced over 10,000 families, leaving them in urgent need of shelter and aid.",
+      date: "2024-12-24",
+    },
+    
+  ];
+
+  return (
+    <div>
+      <div className="home_container">
+        {feeds.map((feed, index) => (
+          <HomeFeed
+            key={index}
+            place={feed.place}
+            image={feed.image}
+            disaster={feed.disaster}
+            description={feed.description}
+            date={feed.date}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
