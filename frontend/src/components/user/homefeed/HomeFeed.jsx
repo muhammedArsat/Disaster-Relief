6d28d4a5357b0d72063a877d5d1a40208943209c
hamsa-09 <hamsavardhinibaskar@gmail.com>
2024-12-26 15:52:30 +0530

    home feed animation

--- frontend/src/components/user/homefeed/HomeFeed.jsx
+++ frontend/src/components/user/homefeed/HomeFeed.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState ,useEffect } from 'react';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 import "./HomeFeed.css";
 import { MdOutlineMyLocation } from "react-icons/md";
 import { PiMoneyFill } from "react-icons/pi";
@@ -7,7 +9,9 @@ import { useNavigate } from 'react-router-dom';
 const homeFeed = ({place,image,description,date}) => {
   const navigate=useNavigate();
   const [isExpanded,setIsExpanded]=useState(false);
-
+  useEffect(()=>{
+    Aos.init({duration:1500});
+  },[]);
   const handleLocationNavigate=()=>{
     navigate("/user/location")
   }
@@ -19,19 +23,19 @@ const homeFeed = ({place,image,description,date}) => {
   }
   return (
     <div className="home-feed">
-    <div className='feed_post'>
+    <div className='feed_post' data-aos="fade-right">
         <h2>{place}</h2>
         <img src={image} alt="Feeds"/>
         <div className='icons'>
         <MdOutlineMyLocation  onClick={handleLocationNavigate}/>
          <PiMoneyFill onClick={handleFundNavigate}/>
         </div>
-      
+
         <p className='description'>
-          {isExpanded?description:`${description.substring(0,100)}...`}
+          {isExpanded?description:`${description.substring(0,100)}`}
           {description.length>100 && (
-            <span className='read-more' onClick={toggleReadMore}>
-              {isExpanded?"Read less":"Read more"}
+            <span clcssName='read-more' onClick={toggleReadMore}>
+              {isExpanded?"Read less":"Read more..."}
             </span>
           )}
           </p>
